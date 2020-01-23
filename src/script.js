51df80ac410fadfa957707462fcdab78b441aaba
Kungfu Dojo <kungfu-dojo@localhost>
2020-01-23 20:36:03 +0200

    add track action

---
+++ src/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $('[data-action]').click(function () {
+    var data = $(this).data('action')
+    gtag('event', 'action', {
+      'event_category': data.category,
+      'event_label': data.label,
+      'value': data.value,
+    })
+  })
+
+  $('.fotorama').on('fotorama:ready ' +           // Fotorama is fully ready
+    'fotorama:show ' +            // Start of transition to the new frame
+    //'fotorama:showend ' +         // End of the show transition
+    //'fotorama:load ' +            // Stage image of some frame is loaded
+    'fotorama:error ' +           // Stage image of some frame is broken
+    'fotorama:startautoplay ' +   // Slideshow is started
+    'fotorama:stopautoplay ' +    // Slideshow is stopped
+    'fotorama:fullscreenenter ' + // Fotorama is fullscreened
+    'fotorama:fullscreenexit ' +  // Fotorama is unfullscreened
+    'fotorama:loadvideo ' +       // Video iframe is loaded
+    'fotorama:unloadvideo',       // Video iframe is removed
+    function (e, fotorama, extra) {
+      gtag('event', 'action', {
+        'event_category': e.type.split(':')[1],
+        'event_label': 'photo',
+        'value': fotorama.activeFrame.img,
+      })
+      //console.log('## ' + e.type)
+      //console.log('active frame', fotorama.activeFrame)
+      //console.log('additional data', extra)
+    }
+  )
+})
+
